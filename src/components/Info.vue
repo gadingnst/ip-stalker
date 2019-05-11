282745f3df21723d7a875265f3f08edd902755a4
Sutan Nst <sutan.gnst@gmail.com>
2019-05-11 13:48:02 +0700

    add embedded map

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="form">
+    <Map v-if="Object.keys(info).length > 0" :latitude="info.latitude.value || 0" :longitude="info.longitude.value || 0" />
     <fieldset v-for="(data, i) in info" class="form-group" :key="i">
       <label>{{ data.label }}</label>
       <input
@@ -16,6 +17,9 @@
 import { mapState } from 'vuex'
 
 export default {
+  components: {
+    Map: () => import('./Map')
+  },
   computed: {
     ...mapState({
       info: state => state.ipInfo
---
+++ src/components/Map.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="text-align: center">
+    <h2 style="margin: 0; color: #4CAF50">Lokasi IP</h2>
+    <iframe 
+      width="300" 
+      height="170" 
+      frameborder="0" 
+      scrolling="no" 
+      marginheight="0" 
+      marginwidth="0" 
+      :src="`https://maps.google.com/maps?q=${latitude},${longitude}&hl=es;z=14&amp;output=embed`"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    latitude: {
+      type: Number,
+      default: 0
+    },
+    longitude: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+iframe {
+  width: 350px;
+  height: 300px;
+  margin: 5px;
+}
+</style>
