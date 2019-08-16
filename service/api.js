fe52245f7bb615564afca54bdff5ec5730bbffda
Sutan Gading Fadhillah Nasution <sutan.gnst@gmail.com>
2019-08-16 15:31:07 +0700

    add try-catch block

--- service/api.js
+++ service/api.js
@@ -21,14 +21,22 @@ route.get('/:addr', async (req, res) => {
     .match(/((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/)
 
   if (valid) {
-    const result = await Promise.all(endpoints(req.params.addr).map(endpoint => (
-      fetch(endpoint).then(({ data }) => data)
-        .catch(({ response }) => console.log(response.data.message))
-    )))
-    
-    res.status(200).send(result.reduce((acc, cur) => ({
-      ...acc,...cur
-    })))
+    try {
+      const result = await Promise.all(endpoints(req.params.addr).map(endpoint => (
+        fetch(endpoint).then(({ data }) => data)
+          .catch(({ response }) => console.log(response.data.message))
+      )))
+      
+      res.status(200).send(result.reduce((acc, cur) => ({
+        ...acc,...cur
+      })))
+    } catch (err) {
+      console.error(err)
+      res.status(500).send({
+        code: 500,
+        message: 'Internal server error'
+      })
+    }
   } else {
     res.status(400).send({
       message: 'Request is not valid IP address'
